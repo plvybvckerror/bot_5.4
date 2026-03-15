92eecde97abaa179cf4962e33a877e9bd04e72d5
family <@matkovskimark@gmail.com>
2026-03-15 15:29:23 +0300

    base bot

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+
+import asyncio
+from aiogram import Bot, Dispatcher
+
+
+bot = Bot(token="8729617934:AAHw2RkCeAk01QnQrTb_O_CzH_bwxdLBwbw")
+dp = Dispatcher()
+
+
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
